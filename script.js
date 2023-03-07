a6d031568a66ec14f6a0e170bc20abd096628d97
JoyObaidu <joyobaidu@gmail.com>
2023-03-07 12:43:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById('nav-menu').style.display = "block";
+    document.querySelector('body').style.backgroundColor =  "hsl(0, 0%, 41%)";
+}
+
+function closeNav(){
+    document.getElementById('nav-menu').style.display = "none";
+    document.querySelector('body').style.backgroundColor =  "hsl(0, 0%, 98%)";
+}
+
